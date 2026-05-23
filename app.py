094a7100975c810628b0869198a9a5951fcf4635
nauistkh0417-cmyk <nau.ist.kh0417@gmail.com>
2026-05-23 10:41:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ st.subheader("📈 Monthly Expenses")
 st.caption("Made with using❤️Streamlit")
 st.write("Hello, *World!*😁")
 
-title st.text_input("Movie title", "Life of Brian") 17
+title = st.text_input("Movie title", "Life of Brian") 17
 st.write("The current movie title is", title)
 
 if st.button("Say hello"): 
     st.write("Why hello there")
 else:
-    st.write("Goodbye"))
+    st.write("Goodbye")
 
 st.button "Reset", type-"primary"
